7bf37627a0f869107eaccdef6e3921ddb30bd22f
Young Trep <64561735+YoungTrep@users.noreply.github.com>
2021-01-28 16:16:27 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='Amazoned',
+    version='0.1',
+    author='youngtrep',
+    author_email='youngtrep.business@gmail.com',
+    description='Amazon link to affiliate link',
+    long_description='Change an Amazon link to an affiliate link',
+    url='https://github.com/YoungTrep/Python-Amazoned',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.6'
+)
